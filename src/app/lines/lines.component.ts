55a06d6e782fa5f9326fe3edd78aeb7cf8fa240c
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2023-01-11 20:30:21 -0300

    added form to add lines

--- src/app/lines/lines.component.ts
+++ src/app/lines/lines.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Line } from "./Line";
 
 @Component({
   selector: 'app-lines',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./lines.component.css']
 })
 export class LinesComponent {
+  lines: Line[] = [];
+  default_color: string = "rgba(0, 150, 0, 0.7)"
 
+  constructor(){
+    
+  }
+
+  // Atención: input es el elemento HTML del form
+  addLine(input: HTMLInputElement) {
+    let text = input.value.trim()
+
+    if (text){
+      this.lines.push({name: text, color: this.default_color})
+
+      // clear form
+      input.value = ""
+    }
+
+    // set cursor on input again
+    input.focus()
+
+    // prevent default
+    return false
+  }
+
+  deleteLine(str: string){
+    for (let i = 0; i < this.lines.length; i++) {
+      const element: Line = this.lines[i];
+      if (element.name == str) this.lines.splice(i, 1)
+    }
+  }
 }
