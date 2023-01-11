cbdc5825a4e01c814ed922094358e68b3b687dce
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2023-01-11 19:02:45 -0300

    nuevos componentes; header y footer comunes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,17 @@ import { AppComponent } from './app.component';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
 import { LinesComponent } from './lines/lines.component';
 import { HomeComponent } from './home/home.component';
+import { HistoryComponent } from './history/history.component';
+import { MonthSummaryComponent } from './month-summary/month-summary.component';
+import { GamesComponent } from './games/games.component';
 
 // my routes
 const routes: Route[] = [
   {path: '', component: HomeComponent},
-  {path: 'lines', component: LinesComponent}
+  {path: 'lines', component: LinesComponent},
+  {path: 'month_summary', component: MonthSummaryComponent},
+  {path: 'history', component: HistoryComponent},
+  {path: 'games', component: GamesComponent}
 ]
 
 
@@ -22,7 +28,10 @@ const routes: Route[] = [
     AppComponent,
     NavHeaderComponent,
     LinesComponent,
-    HomeComponent
+    HomeComponent,
+    HistoryComponent,
+    MonthSummaryComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/month-summary/month-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-month-summary',
+  templateUrl: './month-summary.component.html',
+  styleUrls: ['./month-summary.component.css']
+})
+export class MonthSummaryComponent {
+
+}
